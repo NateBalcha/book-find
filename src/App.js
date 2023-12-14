4009a44a4107d3f7422d5f98a01c5c440e43e8bf
NateBalcha <natebalcha@gmail.com>
2023-12-13 20:50:39 -0500

    navbar changed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-// import "./App.css";
-// import ReactDom from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { AppProvider } from "./context.";
-// import Home from "./pages/Home/Home";
-// import About from "./pages/About/About";
-// import BookList from "./components/BookList/BookList";
-// import BookDetails from "./components/BookDetails/BookDetails";
+import "./App.css";
+import ReactDom from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context.";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
-// function App() {
-//   return (
-//     <div>
-//       <AppProvider>
-//         <BrowserRouter>
-//           <Routes>
-//             <Route path="/" element={<Home />}>
-//               <Route path="about" element={<About />} />
-//               <Route path="book" element={<BookList />} />
-//               <Route path="/book/:id" element={<BookDetails />} />
-//             </Route>
-//           </Routes>
-//         </BrowserRouter>
-//       </AppProvider>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div>
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path="about" element={<About />} />
+              <Route path="book" element={<BookList />} />
+              <Route path="/book/:id" element={<BookDetails />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
